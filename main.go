113ee15dcfc577443be322975d8dd3df34c75110
MeigaraJuma <meigara.juma@gmail.com>
2021-07-20 02:52:21 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"go-gin-tutorial/Config"
+	"go-gin-tutorial/Models"
+	"go-gin-tutorial/Routes"
+
+	"github.com/jinzhu/gorm"
+)
+
+var err error
+
+func main() {
+	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	if err != nil {
+		fmt.Println("Status:", err)
+	}
+	defer Config.DB.Close()
+	Config.DB.AutoMigrate(&Models.User{})
+	r := Routes.SetupRouter()
+	//running
+	r.Run()
+}
